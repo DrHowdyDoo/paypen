87cb4ec39db54022cadd8cb1ca38f25f830aa76c
DrHowdyDoo <55204060+DrHowdyDoo@users.noreply.github.com>
2022-05-01 14:14:39 +0530

    icon added

---
+++ src/Components/Home.js
@@ -0,0 +1,24 @@
+import './Home.css';
+
+const Home = () => {
+
+    return (
+        <div className="container">
+
+            <div className="h-left">
+
+            </div>
+
+            <div className="h-right">
+                <h1>
+                    Welcome to
+                </h1>
+                <h1>PayPen</h1>
+            </div>
+
+        </div>
+    );
+
+}
+
+export default Home;
