88906bbe1693078e0d44524fc5c3888c19a9bee2
DrHowdyDoo <55204060+DrHowdyDoo@users.noreply.github.com>
2022-05-02 10:00:03 +0530

    login-form-improved

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,9 @@ import quote from './Assests/ic_quote.svg';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
 import LoginIcon from '@mui/icons-material/Login';
+import { Checkbox, Typography } from '@mui/material';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { borderRadius, fontSize, fontWeight } from '@mui/system';
 
 const Home = () => {
 
@@ -27,7 +30,14 @@ const Home = () => {
                         <TextField fullWidth className='inputRounded' id="password" label="Password" variant="outlined" margin="normal"
                             type="password"
                         />
-                        <Button className="btn-login" size="large" variant="contained" style={{ borderRadius: 50, marginTop: 24, backgroundColor: "#1f28eb" }}>Login</Button>
+                        <div className="form-control">
+                            <FormControlLabel control={<Checkbox defaultChecked style={{ color: "#1f28eb" }} />} label={<span style={{ fontWeight: "600", fontSize: "15px" }}>Remember me</span>} />
+                            <span>
+                                <Button variant="text" style={{ textTransform: 'none', fontWeight: "600", color: "#1f28eb", marginLeft: "62px", borderRadius: 50 }}>Forgot Password?</Button>
+                            </span>
+                        </div>
+
+                        <Button className="btn-login" size="large" variant="contained" style={{ borderRadius: 50, marginTop: 24, backgroundColor: "#1f28eb", textTransform: 'none', fontWeight: "600" }}>Login</Button>
                     </form>
 
                 </div>
@@ -39,11 +49,11 @@ const Home = () => {
                 <div className="title-wrapper">
                     <img src={quote} alt="quote_icon" />
                     <div className="l-top"></div>
-                    <h1 className="title">
-                        Do <span className="sub-quote">something,</span> <br />
-                        that <span className="sub-quote">your future</span> <br />
-                        self will <span className="sub-quote">thank</span> <br />
-                        you <span className="sub-quote">for.</span>
+                    <h1 className="quote">
+                        Don't let <span className="sub-quote">retirement</span> <br />
+                        put <span className="sub-quote">an end</span> <br />
+                        to <span className="sub-quote">your</span> <br />
+                        <span className="sub-quote">finances.</span>
                     </h1>
                 </div>
                 <div className="l-bottom"></div>
